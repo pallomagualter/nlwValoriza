aeadff9894e41b10e9b15f3815cff52051098646
pallomagualter <pallomagualter@gmail.com>
2021-06-24 18:12:15 -0300

    third day

---
+++ src/services/CreateTagsService.ts
@@ -0,0 +1,33 @@
+import { getCustomRepository } from "typeorm";
+import { TagsRepositories } from "../repositories/TagsRepositories";
+
+
+class CreateTagsService {
+    async execute(name: string) {
+        const tagsRepositories = getCustomRepository(TagsRepositories);
+
+        if (!name) { //verificação se está passando name
+            throw new Error("Incorrent name!");
+        }
+
+        // verifica se já existe este nome cadastrado
+        // SELECT * FROM TAGS WHERE NAME = 'name'
+        const tagAlreadyExists = await tagsRepositories.findOne({ 
+            name
+        });
+
+        if (tagsRepositories) {
+            throw new Error("Tag already exists!");
+        }
+
+        const tag = tagsRepositories.create({
+            name
+        });
+
+        await tagsRepositories.save(tag);
+
+        return tag;
+    }
+}
+
+export { CreateTagsService }
