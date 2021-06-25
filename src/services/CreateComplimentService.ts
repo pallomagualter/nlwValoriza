c438ec665a0b7d9b988253a82e81c6b460052c49
pallomagualter <pallomagualter@gmail.com>
2021-06-25 20:51:09 -0300

    Fourth Day

--- src/services/CreateComplimentService.ts
+++ src/services/CreateComplimentService.ts
@@ -12,10 +12,36 @@ interface IComplimentRequest {
 }
 
 class CreateComplimentService {
-    async execute({tag_id, user_sender, user_receiver, message}: IComplimentRequest) {
+    async execute({
+        tag_id, 
+        user_sender, 
+        user_receiver, 
+        message,
+    }: IComplimentRequest) {
         const complimentsRepositories = getCustomRepository(ComplimentsRepositories);
         const usersRepositories = getCustomRepository(UsersRepositories);
+
+        if(user_sender === user_receiver) {
+            throw new Error("Incorrect User Receiver");
+        }
+
+        const userReceiverExists = await usersRepositories.findOne(user_receiver);
+
+        if (!userReceiverExists) {
+            throw new Error("User Receiver does not exists!");
+        }
+
+        const compliment = complimentsRepositories.create({
+            tag_id,
+            user_receiver,
+            user_sender,
+            message,
+        });
+
+        await complimentsRepositories.save(compliment);
+
+        return compliment;
+    }   
 }
 
 export { CreateComplimentService }
-//1:11
