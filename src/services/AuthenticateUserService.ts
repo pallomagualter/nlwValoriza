3ea3a35953522de024a05894fc3fa1c83e715bb9
pallomagualter <pallomagualter@gmail.com>
2021-06-25 12:28:18 -0300

    Fourth Day

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,6 +1,7 @@
 import { getCustomRepository } from "typeorm";
 import { UsersRepositories } from "../repositories/UsersRepositories";
 import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
 
 interface IAuthenticateRequest {
@@ -22,11 +23,21 @@ class AuthenticateUserService {
         }
         // Verificar se senha está correta
         const passwordMatch = await compare(password, user.password);
+        // sempre que houver retorno de uma promise é necessário o await
 
         if(!passwordMatch) {
             throw new Error("Email/Password incorrect")
         }
         // Gerar token
+        const token = sign({
+                email: user.email
+            }, "a0a68817a5ac9b36bddada1fd86be7d6", {
+                subject: user.id,
+                expiresIn: "1d",
+            }
+        );
+
+        return token;
     }
 }
 
