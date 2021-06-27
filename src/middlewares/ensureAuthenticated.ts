8779bdd6bc2b103aef37b42604f0b3c56824a1cc
pallomagualter <pallomagualter@gmail.com>
2021-06-27 12:34:31 -0300

    Fifth Day

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,24 +1,39 @@
 import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+interface IPayLoad {
+    sub: string;
+}
 
 export function ensureAuthenticated(
     request: Request,
     response: Response,
     next: NextFunction
-){
+    ){
     // Receber o token
-    const token = request.headers.authorization;
-    //console.log(token);
+    const authToken = request.headers.authorization;
     
     // Validar se token está preenchido
-    if (!token) {
+    if (!authToken) {
         return response.status(401).end();
     }
+    //console.log(token);
 
-    return next();
+    // vai gerar um array dividindo pelo espaço e na primeira posição vai ignorar e a segunda armazenar na variável token
+    const [, token] = authToken.split(" "); 
 
+    // Validar se token é válido 
+    try {
+        const { sub }= verify( 
+            token, 
+            "a0a68817a5ac9b36bddada1fd86be7d6"
+        ) as IPayLoad;
 
-    // Validar se token é válido
-
-    // Recuperar informações do usuário
-
+        // Recuperar informações do usuário
+        request.user_id = sub;
+        
+        return next();
+    } catch (err) {
+        response.status(401).end();
+    }   
 }
